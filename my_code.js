9e3639b00e342f401141338a079d35c4797b1bf6
John Peterson <jlpeterson829@gmail.com>
2015-01-23 12:24:04 -0800

    Add non-user friendly time stamp

--- my_code.js
+++ my_code.js
@@ -2,15 +2,15 @@
 
  $(document).ready(function(){
         var $body = $('body');
-
+        var $newsFeed = $('.news-feed')
         $('button').click(function(){
         var index = streams.home.length - 1;
         while(index >= 0){
           var tweet = streams.home[index];
           var $tweet = $('<div></div>');
-          var time_created = 
-          $tweet.text('@' + tweet.user + ': ' + tweet.message);
-          $tweet.appendTo($body);
+          var time_created = new Date();
+          $tweet.text('@' + tweet.user + ': ' + tweet.message + '. -' + time_created);
+          $tweet.prependTo($newsFeed);
           index -= 1;
         }
     });
