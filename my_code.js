926eccc7e79686bf7adef0481cbf13f5b605cf3f
John Peterson <jlpeterson829@gmail.com>
2015-01-26 16:46:18 -0800

    Add one tweet to top of the page at a time

--- my_code.js
+++ my_code.js
@@ -1,17 +1,34 @@
 // javascript for twittler
 
- $(document).ready(function(){
-        var $body = $('body');
-        var $newsFeed = $('.news-feed')
-        $('button').click(function(){
-        var index = streams.home.length - 1;
-        while(index >= 0){
+ var main = function(){
+    var $body = $('body');
+    var $newsFeed = $('.news-feed')
+
+
+    
+
+    var getTweet = function(){
+       var index = streams.home.length - 1;
+        for(var i = 0; i<index; i++){
           var tweet = streams.home[index];
           var $tweet = $('<div></div>');
-          var time_created = new Date();
-          $tweet.text('@' + tweet.user + ': ' + tweet.message + '. -' + time_created);
+          var created_at = (new Date ());
+          $tweet.text('@' + tweet.user + ': ' + tweet.message + ' - ' +created_at);
           $tweet.prependTo($newsFeed);
-          index -= 1;
+          index += 1;
         }
+    }
+
+    var removeTweet = function(){
+      var index = streems.home.length-1;
+      while (index>=0){
+        var tweet = streams.home[index]
+      }
+    }
+
+    $('button').click(function(){
+       $('.tweets').text(getTweet()).prependTo($newsFeed);
     });
- });
+ }
+
+$(document).ready(main);
